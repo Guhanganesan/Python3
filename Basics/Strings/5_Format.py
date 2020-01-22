e758d7b456f32183e18e6bd81e0e80a363694027
Guhan Ganesan <bharadhiguhan@ymail.com>
2020-01-22 10:50:37 +0530

    Update 5_Format.py

--- Basics/Strings/5_Format.py
+++ Basics/Strings/5_Format.py
@@ -16,3 +16,22 @@ print("His name is {name}".format(name="Guhan"))
 for i in range(1,11):
     print("{} x 8 = {}".format(i,(i*8)))
 """
+
+
+
+
+brightness_4
+# Formatting of Integers 
+String1 = "{0:b}".format(16) 
+print("\nBinary representation of 16 is ") 
+print(String1) 
+  
+# Formatting of Floats 
+String1 = "{0:e}".format(165.6458) 
+print("\nExponent representation of 165.6458 is ") 
+print(String1) 
+  
+# Rounding off Integers 
+String1 = "{0:.2f}".format(1/6) 
+print("\none-sixth is : ") 
+print(String1) 
