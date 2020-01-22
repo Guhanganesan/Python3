d09996cf297c4129e8fea77e319ec56f89b43255
Guhan Ganesan <bharadhiguhan@ymail.com>
2020-01-22 10:25:25 +0530

    Create print.py

---
+++ Basics/Operators/print.py
@@ -0,0 +1,12 @@
+print("Guhan Ganesan") 
+  
+x = 5
+# Two objects are passed 
+print("x =", x) 
+  
+# code for disabling the softspace feature  
+print('G', 'F', 'G', sep ='') 
+  
+# using end argument 
+print("Python", end = '@')   
+print("Guhan Ganesan")  
