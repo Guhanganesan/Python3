87c245c82b73eeb9663de2d4f0d68cb4d70589c2
Guhan Ganesan <bharadhiguhan@ymail.com>
2020-05-06 20:03:50 +0530

    Create Argument.py

---
+++ Basics/Operators/Argument.py
@@ -0,0 +1,11 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("x", help="Enter number 1")
+parser.add_argument("y", help="Enter number 2")
+args = parser.parse_args()
+print(args.x+args.y)
+"""
+B:\Python2020-master\Basics\Operators>python Argument.py 10 20
+1020
+"""
+
