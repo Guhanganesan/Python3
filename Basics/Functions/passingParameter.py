fd0d92fd04d900ca5b9dbd17811a1882470dfba8
Guhan Ganesan <phpguhan@gmail.com>
2020-10-11 14:08:02 +0530

    Function calling and parameter passing

---
+++ Basics/Functions/passingParameter.py
@@ -0,0 +1,32 @@
+x = 10
+y = 20
+def add(a,b):
+    x = a
+    y = b
+    print(x+y) #100
+
+add(40,60)
+print(x+y) #30
+
+class Test:
+    x = 10
+    y = 20
+    def __init__(self):
+        print("Welcome")
+    
+    def add(self, a, b):
+        self.x = a
+        self.y = b
+        print(self.x + self.y)
+
+    def __str__(self):
+        self.x = 300
+        self.y = 400
+        print(self.x + self.y)
+
+obj = Test()
+obj.add(50,70)
+obj.__str__()
+
+
+
