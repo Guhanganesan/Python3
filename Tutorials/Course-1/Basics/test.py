c1b3011ede662eab651e69241508cec34f3e1574
Guhan Ganesan <phpguhan@gmail.com>
2023-03-12 21:14:49 +0530

    This is for learning purpose

---
+++ Tutorials/Course-1/Basics/test.py
@@ -0,0 +1,5 @@
+# compiler => translate and convert into system format ex: print("Hello World")
+# interpreter => follow the instructions (programming) read , write, add line by line 
+# code => compiler => check syntax and convert into machine code => interpreter => performing as per the inst 
+print("Hello") 
+print(10+12)
