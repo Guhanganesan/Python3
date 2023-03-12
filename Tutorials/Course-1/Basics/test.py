c01e28e47ecce870741bdaf7c86fe0ea77f4811d
Guhan Ganesan <phpguhan@gmail.com>
2023-03-12 22:24:03 +0530

    basics1

--- Tutorials/Course-1/Basics/test.py
+++ Tutorials/Course-1/Basics/test.py
@@ -1,5 +1,25 @@
 # compiler => translate and convert into system format ex: print("Hello World")
 # interpreter => follow the instructions (programming) read , write, add line by line 
 # code => compiler => check syntax and convert into machine code => interpreter => performing as per the inst 
+# Additional compiler is not necessary for interpreted programming language
 print("Hello") 
 print(10+12)
+
+#python test.py
+
+# php, python, JavaScript, Perl (do not depends on the datatype but we wherever it is)
+x=1045598484058
+y=20.5
+print(x+y)
+
+# programming language like C, java, c++, .net  it depends on the datatypes (strictly)
+# javac test.java => test.class => java test
+"""
+#include <stdio.h>
+int main()
+{
+    int x=10; //depends on the datatypes
+    printf("Hello World");
+    return 0;
+}
+"""
