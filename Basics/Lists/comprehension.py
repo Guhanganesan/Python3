a7ada3fffab9c56ce154afb9fceeb0ca9ef93bf0
Guhan Ganesan <phpguhan@gmail.com>
2024-06-29 16:33:58 +0530

    Update comprehension.py

--- Basics/Lists/comprehension.py
+++ Basics/Lists/comprehension.py
@@ -12,3 +12,36 @@ print(z) # ['g10', 'u10', 'h10', 'a10', 'n10']
 lis = [i**2 if i % 2 == 0 
        else i**3 for i in range(8)] 
 print(lis) # [0, 1, 4, 27, 16, 125, 36, 343]
+
+string = 'GuhanGanesan'
+# Toggle case of each character 
+# a = 97
+# A = 65 
+# Diff = 97-65 = 32
+# ^ => (-65) = 65
+List = list(map(lambda i: chr(ord(i)^32), string))
+print("".join(List)) # gUHANgANESAN
+
+names = ["G", "G", "g"] 
+ages = [25, 30, 35] 
+person_tuples = [ (name, str(age)) for name, age in zip(names, ages)] 
+print(person_tuples) # [('G', '25'), ('G', '30'), ('g', '35')]
+
+x=('a','b')
+y="".join(x)
+print(y) # ab 
+
+z = "".join(["".join(i) for i in person_tuples])
+print(z) # G25G30g35
+
+person_dict = [{key:val} for key, val in zip(names,ages)]
+print(person_dict) #[{'G': 25}, {'G': 30}, {'g': 35}]
+
+z=dict(zip(names,ages))
+print(z) # {'G': 30, 'g': 35} duplicate key removed
+
+
+
+
+
+
