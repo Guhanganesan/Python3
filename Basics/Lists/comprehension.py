c9a21c291a13ae3900528ef5bac4d2f948bdfcb6
Guhan Ganesan <phpguhan@gmail.com>
2024-06-29 13:18:07 +0530

    Update comprehension.py

--- Basics/Lists/comprehension.py
+++ Basics/Lists/comprehension.py
@@ -5,3 +5,10 @@ print("".join(z))
 
 # 1g2u3h4a5n
 
+a='guhan'
+z=list(map(lambda char: char+'10', [char for char in a]))
+print(z) # ['g10', 'u10', 'h10', 'a10', 'n10']
+
+lis = [i**2 if i % 2 == 0 
+       else i**3 for i in range(8)] 
+print(lis) # [0, 1, 4, 27, 16, 125, 36, 343]
