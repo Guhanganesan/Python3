519179f88bac98ecef6dd89cad890354afa3453d
Guhan Ganesan <phpguhan@gmail.com>
2024-06-30 18:38:41 +0530

    Create Reduce.py

---
+++ Basics/Lambda/Reduce.py
@@ -0,0 +1,82 @@
+# python code to demonstrate working of reduce()
+
+# importing functools for reduce()
+import functools
+
+# initializing list
+lis = [1, 3, 5, 6, 2]
+
+# using reduce to compute sum of list
+print("The sum of the list elements is : ", end="")
+print(functools.reduce(lambda a, b: a+b, lis))
+
+# using reduce to compute maximum element from list
+print("The maximum element of the list is : ", end="")
+print(functools.reduce(lambda a, b: a if a > b else b, lis))
+
+# The sum of the list elements is : 17
+# The maximum element of the list is : 6
+
+# python code to demonstrate working of reduce()
+# using operator functions
+
+# importing functools for reduce()
+import functools
+
+# importing operator for operator functions
+import operator
+
+# initializing list
+lis = [1, 3, 5, 6, 2]
+
+# using reduce to compute sum of list
+# using operator functions
+print("The sum of the list elements is : ", end="")
+print(functools.reduce(operator.add, lis))
+
+# using reduce to compute product
+# using operator functions
+print("The product of list elements is : ", end="")
+print(functools.reduce(operator.mul, lis))
+
+# using reduce to concatenate string
+print("The concatenated product is : ", end="")
+print(functools.reduce(operator.add, ["Ab", "Cd", "Ef"]))
+
+# The sum of the list elements is : 17
+# The product of list elements is : 180
+# The concatenated product is : AbCdEf
+
+# importing itertools for accumulate()
+import itertools
+
+# importing functools for reduce()
+import functools
+
+# initializing list
+lis = [1, 3, 4, 10, 4]
+
+# printing summation using accumulate()
+print("The summation of list using accumulate is :", end="")
+print(list(itertools.accumulate(lis, lambda x, y: x+y)))
+
+# printing summation using reduce()
+print("The summation of list using reduce is :", end="")
+print(functools.reduce(lambda x, y: x+y, lis))
+
+# The summation of list using accumulate is :[1, 4, 8, 18, 22]
+# The summation of list using reduce is :22
+
+#==================== Simply ===========================
+
+from functools import reduce
+# Example: Sum of all elements
+numbers = [1, 2, 3, 4]
+result = reduce(lambda x, y: x + y, numbers)
+print(result)  # Output: 10
+
+
+
+
+
+
