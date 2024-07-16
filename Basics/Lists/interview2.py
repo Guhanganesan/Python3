3aaf29ce4421454d05f46fac05dbd7b2ebdee73a
guhanganesan <phpguhan@gmail.com>
2024-07-16 17:43:32 +0530

    updated list

--- Basics/Lists/interview2.py
+++ Basics/Lists/interview2.py
@@ -1,17 +1,12 @@
-# [(1,6),(2,4),(9,7),(5,3)]
-# Requirement:: [(9,7),(5,3),(1,6),(2,4)]
+# Input : [(1,6),(2,4),(9,7),(5,3)]
+# Output: [(9,7),(5,3),(1,6),(2,4)]
 
-temp=0
 L=[(1,6),(2,4),(9,7),(5,3)]
-res=[]
+temp=0
 for i in range(0, len(L)):
-    # print(sum(data))
-    # if sum(data) > temp:
-    #     temp=data
     for j in range(0, len(L)):
         if sum(L[i]) > sum(L[j]):
             temp=L[i]
             L[i]=L[j]
             L[j]=temp
-            
 print(L)
